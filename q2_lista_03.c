63b03352e07b27ed575e714157ec708ece699a6a
cgustavoa <142316756+cgustavoa@users.noreply.github.com>
2023-11-14 11:15:50 -0300

    FINALIZADA

--- q2_lista_03.c
+++ q2_lista_03.c
@@ -1,27 +1,19 @@
 #include <stdio.h>
 #include <math.h>
 
-// ERRO NO CÁLCULO FINAL L:22;
 int main(){
     double investimento_inicial, taxa_retorno_mensal, aporte_mensal, valor_montante;
     int meses;
 
-    printf("Informe a quantidade de meses: ");
     scanf("%d", &meses);
-
-    printf("Informe o investimento mensal: ");
     scanf("%lf", &aporte_mensal);
-
-    printf("Informe a taxa de juros mensal (em porcentagem): ");
     scanf("%lf", &taxa_retorno_mensal);
 
-    // Convertendo a taxa de juros para decimal
-    taxa_retorno_mensal = taxa_retorno_mensal / 100.0;
-
-    for (int i = 1; i <= meses; i++) {
-        valor_montante = aporte_mensal * (1 + taxa_retorno_mensal) * (pow((1 + taxa_retorno_mensal), meses) - 1 / meses) + aporte_mensal;
-        printf("Ao fim do mês %d, o montante é: %.2lf\n", i, valor_montante);
+    for (int i = 1; i <= meses; i++){
+        valor_montante = aporte_mensal *(1 + taxa_retorno_mensal) * (((pow(1 + taxa_retorno_mensal, i) - 1)/taxa_retorno_mensal));
+        printf("Montante ao fim do mes %d: R$ %.2lf\n", i, valor_montante);
     }
 
     return 0;
 }
+
